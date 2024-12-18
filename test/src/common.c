1f6fab7fece9c473efaa6e72c37891177796eff1
Caio Renato Bedulli do Carmo <caio.bedulli@gmail.com>
2024-12-18 20:18:53 -0300

    Make test code be ANSI compliant

--- test/src/common.c
+++ test/src/common.c
@@ -4,10 +4,11 @@ void *malloca(size_t bytes); /* bytes must be <= 0x0000FFFF*/
 void malloca_free(void *ptr);
 
 void print_sep(void) {
+    char msg[] = "-----------------------------------------------------------------------------------\n\n";
+    int fd;
+
     if (!DEBUG_ENABLED) { return; }
 
-    int fd;
-    char msg[] = "-----------------------------------------------------------------------------------\n\n";
     fd = open("/dev/stdout", O_WRONLY);
     write(fd, msg, sizeof(msg) - 1 /* exclude \0 */);
     close(fd);
@@ -19,18 +20,17 @@ void _malloca_free(void *ptr) {
 }
 
 void new_line(void) {
-    if (!DEBUG_ENABLED) { return; }
-
     char msg[] = "\n";
     int fd;
+
+    if (!DEBUG_ENABLED) { return; }
+
     fd = open("/dev/stdout", O_WRONLY);
     write(fd, msg, sizeof(msg) - 1 /* excludes \0*/);
     close(fd);
 }
 
 void print_got_from_malloqueiro(void *ptr) {
-    if (!DEBUG_ENABLED) { return; }
-
     /* 32 bit. Note that if this assert fails, it will try to call
      * malloc and the error may be hidden!
      * */
@@ -38,6 +38,8 @@ void print_got_from_malloqueiro(void *ptr) {
     uint8_t msg_len;
     int fd;
     /* if msg was 61, we could get aligned to 64 bytes */
+
+    if (!DEBUG_ENABLED) { return; }
 
     assert(sizeof ptr == 4);
     msg_len = sprintf(msg, "Got from malloqueiro: %p\n",  ptr);
@@ -53,6 +55,16 @@ void _malloca(void **ptr, uint8_t ptr_offset, uint32_t bytes) {
     print_sep();
 }
 
+__attribute__((naked))
 void *syscall_current_brk(void) {
-    return sbrk(0);
+    /* sbrk is not in ANSI C and I don't want to read
+       from "/proc/self/maps" file. */
+    __asm__ (
+        "push %ebx \n\t"
+        "movl $45, %eax\n\t"
+        "movl $0, %ebx\n\t"
+        "int $0x80\n\t"
+        "pop %ebx \n\t"
+        "ret\n\t"
+    );
 }
